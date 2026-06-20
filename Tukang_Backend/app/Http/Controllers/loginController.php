ae08e700df30a2e6a8adb0f4bcc46dd5c8e3f883
raihan678 <akunnrandom337@gmail.com>
2026-06-20 20:59:57 +0700

    1.bikin alur home dan /login

---
+++ Tukang_Backend/app/Http/Controllers/loginController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class loginController extends Controller
+{
+    
+       public function index()
+    {
+        return view('frontend.login');
+    }
+
+
+}
--- Tukang_Backend/routes/web.php
+++ Tukang_Backend/routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
+use App\Http\Controllers\homeController;
+use App\Http\Controllers\loginController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/', [homeController::class, 'landing'])->name('home.landing');
+Route::get('/login', [loginController::class, 'index'])->name('login');
