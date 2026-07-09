43d8d62f2db995818c5f18ade6b9f381335eea37
raflyhr <raflyhermansyah565@gmail.com>
2026-07-09 22:02:04 +0700

    chore(backend): catch exception to debug 500 error on vercel

--- Tukang_Backend/api/index.php
+++ Tukang_Backend/api/index.php
@@ -2,5 +2,12 @@
 
 define('IS_VERCEL', true);
 
-// Forward Vercel requests to Laravel's index.php
-require __DIR__ . '/../public/index.php';
+try {
+    require __DIR__ . '/../public/index.php';
+} catch (\Throwable $e) {
+    echo "<h1>Fatal Error Caught in api/index.php</h1>";
+    echo "<pre>";
+    echo $e->getMessage() . "\n";
+    echo $e->getTraceAsString();
+    echo "</pre>";
+}
